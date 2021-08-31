d4ea29cd19bf37558cb0561567c139d74fe1cf99
Edgar Malagón <emalagonc@gmail.com>
2021-08-31 18:38:22 -0500

    Indent with spaces.

--- ssd1306/ssd1306.c
+++ ssd1306/ssd1306.c
@@ -122,10 +122,10 @@ int ssd1306_init(const ssd1306_t *dev)
             pin_cfg = 0x02;
             break;
         case 32:
-        	if(dev->width == 128)
-        		pin_cfg = 0x02;
-        	else
-        		pin_cfg = 0x12;
+            if(dev->width == 128)
+                pin_cfg = 0x02;
+            else
+                pin_cfg = 0x12;
             break;
         case 64:
             pin_cfg = 0x12;
@@ -199,12 +199,12 @@ int ssd1306_load_frame_buffer(const ssd1306_t *dev, uint8_t buf[])
     uint8_t tab[16] = { 0 };
     size_t len = dev->width * dev->height / 8;
     if (dev->screen == SSD1306_SCREEN) {
-    	if(dev->width == 64 && dev->height == 32) {
-			ssd1306_set_column_addr(dev, 0x20, 0x20 + (dev->width - 1));
-    	} else {
-			ssd1306_set_column_addr(dev, 0, dev->width - 1);
-    	}
-		ssd1306_set_page_addr(dev, 0, dev->height / 8 - 1);
+        if(dev->width == 64 && dev->height == 32) {
+            ssd1306_set_column_addr(dev, 0x20, 0x20 + (dev->width - 1));
+        } else {
+            ssd1306_set_column_addr(dev, 0, dev->width - 1);
+        }
+        ssd1306_set_page_addr(dev, 0, dev->height / 8 - 1);
     }
 
     for (i = 0; i < len; i++) {
